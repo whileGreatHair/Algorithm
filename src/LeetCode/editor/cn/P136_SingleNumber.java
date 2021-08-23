c1efb5f315b77d9afb11a1dc76017d1c07278a14
GreatHair <whileGreatHair@gmail.com>
2021-08-23 17:38:43 +0800

    提交456题

---
+++ src/LeetCode/editor/cn/P136_SingleNumber.java
@@ -0,0 +1,20 @@
+package LeetCode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//136.只出现一次的数字
+public class P136_SingleNumber {
+        //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        return 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+     public static void main(String[] args) {
+           Solution solution = new P136_SingleNumber().new Solution();
+      }
+}
