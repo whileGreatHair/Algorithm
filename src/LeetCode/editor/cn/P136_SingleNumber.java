8c3e4512ef98b3c0aae2dedc83f8712b9fe8d6c5
GreatHair <whileGreatHair@gmail.com>
2021-08-31 16:21:15 +0800

    提交136题

--- src/LeetCode/editor/cn/P136_SingleNumber.java
+++ src/LeetCode/editor/cn/P136_SingleNumber.java
@@ -5,16 +5,20 @@ import java.util.Map;
 
 //136.只出现一次的数字
 public class P136_SingleNumber {
-        //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int singleNumber(int[] nums) {
-
-        return 1;
+    //leetcode submit region begin(Prohibit modification and deletion)
+    //位运算
+    class Solution {
+        public int singleNumber(int[] nums) {
+            int single = 0;
+            for (int num : nums) {
+                single ^= num;
+            }
+            return single;
+        }
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+    //leetcode submit region end(Prohibit modification and deletion)
 
-     public static void main(String[] args) {
-           Solution solution = new P136_SingleNumber().new Solution();
-      }
+    public static void main(String[] args) {
+        Solution solution = new P136_SingleNumber().new Solution();
+    }
 }
