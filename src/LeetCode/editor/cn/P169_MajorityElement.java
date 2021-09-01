c52eaec73710d8273859365ca6c08e6745e45259
GreatHair <whileGreatHair@gmail.com>
2021-09-01 14:00:02 +0800

    提交169题:哈希表、分治法、投票法

--- src/LeetCode/editor/cn/P169_MajorityElement.java
+++ src/LeetCode/editor/cn/P169_MajorityElement.java
@@ -1,6 +1,8 @@
 package LeetCode.editor.cn;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 //169.多数元素
 public class P169_MajorityElement {
@@ -12,7 +14,93 @@ public class P169_MajorityElement {
             return nums[nums.length / 2];
         }
     }
-//leetcode submit region end(Prohibit modification and deletion)
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    //哈希表
+    class Solution2 {
+        private Map<Integer, Integer> countNums(int[] nums) {
+            Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+            for (int num : nums) {
+                if (!counts.containsKey(num)) {
+                    counts.put(num, 1);
+                } else {
+                    counts.put(num, counts.get(num) + 1);
+                }
+            }
+            return counts;
+        }
+
+        public int majorityElement(int[] nums) {
+            Map<Integer, Integer> counts = countNums(nums);
+
+            Map.Entry<Integer, Integer> majorityEntry = null;
+            for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+                if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                    majorityEntry = entry;
+                }
+            }
+
+            return majorityEntry.getKey();
+        }
+    }
+
+    //分治法
+    class Solution3 {
+        private int countInRange(int[] nums, int num, int lo, int hi) {
+            int count = 0;
+            for (int i = lo; i <= hi; i++) {
+                if (nums[i] == num) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private int majorityElementRec(int[] nums, int lo, int hi) {
+            // base case; the only element in an array of size 1 is the majority
+            // element.
+            if (lo == hi) {
+                return nums[lo];
+            }
+
+            // recurse on left and right halves of this slice.
+            int mid = (hi - lo) / 2 + lo;
+            int left = majorityElementRec(nums, lo, mid);
+            int right = majorityElementRec(nums, mid + 1, hi);
+
+            // if the two halves agree on the majority element, return it.
+            if (left == right) {
+                return left;
+            }
+
+            // otherwise, count each element and return the "winner".
+            int leftCount = countInRange(nums, left, lo, hi);
+            int rightCount = countInRange(nums, right, lo, hi);
+
+            return leftCount > rightCount ? left : right;
+        }
+
+        public int majorityElement(int[] nums) {
+            return majorityElementRec(nums, 0, nums.length - 1);
+        }
+    }
+    //Boyer-Moore 投票算法
+    //摩尔投票法，遇到相同的数，就投一票，遇到不同的数，就减一票，最后还存在票的数就是众数
+    class Solution4 {
+        public int majorityElement(int[] nums) {
+            int count = 0;
+            Integer candidate = null;
+
+            for (int num : nums) {
+                if (count == 0) {
+                    candidate = num;
+                }
+                count += (num == candidate) ? 1 : -1;
+            }
+
+            return candidate;
+        }
+    }
 
     public static void main(String[] args) {
         Solution solution = new P169_MajorityElement().new Solution();
